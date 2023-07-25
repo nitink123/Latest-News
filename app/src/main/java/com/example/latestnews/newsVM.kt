76be4288a816f22790d5de75456e163bf0203e97
NITIN <nitin.b@olaelectric.com>
2023-07-25 11:49:36 +0530

    added comments

--- app/src/main/java/com/example/latestnews/newsVM.kt
+++ app/src/main/java/com/example/latestnews/newsVM.kt
@@ -4,12 +4,10 @@ import android.app.Application
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,42 +18,46 @@ class newsVM(application: Application) :  AndroidViewModel(application)  {
     val newsResponseCategory:MutableLiveData<Response<newsData>> = MutableLiveData()
 
     fun getNews() {
-        val newsApi = NewsService.newInstance.getNews("in")
-        newsApi.enqueue(object : Callback<newsData> {
-            override fun onResponse(call: Call<newsData>, response: Response<newsData>) {
-                Log.e("response","response mai enter kr gaya")
-                val result  = response.body()
-                if(result != null) {
-                   newsResponse.value = response
+        viewModelScope.launch(Dispatchers.IO) {
+            val newsApi = NewsService.newInstance.getNews("in")
+            newsApi.enqueue(object : Callback<newsData> {
+                override fun onResponse(call: Call<newsData>, response: Response<newsData>) {
+                    Log.e("response", "response mai enter kr gaya")
+                    val result = response.body()
+                    if (result != null) {
+                        newsResponse.value = response
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<newsData>, t: Throwable) {
-                Log.e("failure","failure mai enter kr gaya")
+                override fun onFailure(call: Call<newsData>, t: Throwable) {
+                    Log.e("failure", "failure mai enter kr gaya")
 
-              //  Toast.makeText(this@MainActivity,"response is not generated", Toast.LENGTH_LONG)
-            }
+                    //  Toast.makeText(this@MainActivity,"response is not generated", Toast.LENGTH_LONG)
+                }
 
-        })
+            })
+        }
     }
 
     fun getNewsCategoryWise(string: String){
-        val newsApi = NewsService.newInstance.getNewsCate0goryWise("in",string)
-        newsApi.enqueue(object : Callback<newsData> {
-            override fun onResponse(call: Call<newsData>, response: Response<newsData>) {
-                Log.e("response","response mai enter kr gaya")
-                val result  = response.body()
-                if(result != null) {
-                    newsResponseCategory.value = response
+        viewModelScope.launch (Dispatchers.IO) {
+            val newsApi = NewsService.newInstance.getNewsCate0goryWise("in", string)
+            newsApi.enqueue(object : Callback<newsData> {
+                override fun onResponse(call: Call<newsData>, response: Response<newsData>) {
+                    Log.e("response", "response mai enter kr gaya")
+                    val result = response.body()
+                    if (result != null) {
+                        newsResponseCategory.value = response
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<newsData>, t: Throwable) {
-                Log.e("failure","failure mai enter kr gaya")
+                override fun onFailure(call: Call<newsData>, t: Throwable) {
+                    Log.e("failure", "failure mai enter kr gaya")
 
-                //  Toast.makeText(this@MainActivity,"response is not generated", Toast.LENGTH_LONG)
-            }
+                    //  Toast.makeText(this@MainActivity,"response is not generated", Toast.LENGTH_LONG)
+                }
 
-        })
+            })
+        }
     }
 }
